47c33a0004fa9580b1e18fca676dc8c24bb44a6f
Peter Munch-Ellingsen <peterme@peterme.net>
2017-05-12 13:36:17 +0200

    Changed the ticker to a single linked list to allow deletion of nodes.

--- gamelib/ticker.nim
+++ gamelib/ticker.nim
@@ -5,10 +5,12 @@
 # The ticker also keeps track of the time by itself and such doesn't require to
 # be called with a tickLength value. However a version of the tick function
 # exists that overrides the tickLength in order to allow the tickers to be
-# nested.
+# nested. This is practical for things like particle effects where all the
+# particles can be put in a single ticker (which is then added to the main
+# ticker) and then be deleted all at once when the effect is over.
 #
 # Tickers can also be set on pause which means that they won't call the tick
-# procedures but will still keep track of time. This is stop elements from
+# procedures but will still keep track of time. This is to stop elements from
 # trying to catch up after a pause where they were meant to be frozen.
 ################################################################################
 
@@ -21,10 +23,26 @@ type
 
   TickProc* = proc (tickLength: float)
 
+  TickNode* = ref object
+    ticker: TickProc
+    next: TickNode
+
   Ticker = ref object
-    ticks: seq[TickProc]
+    ticks, last: TickNode
     lastTick: float
     paused*: bool
+
+iterator items(ticker:TickNode):TickProc =
+  var t = ticker
+  while t!=nil:
+    yield t.ticker
+    t = t.next
+
+iterator nodes(ticker:TickNode):TickNode =
+  var t = ticker
+  while t!=nil:
+    yield t
+    t = t.next
 
 proc tick*(ticker: Ticker) =
   let currentTick = epochTime()
@@ -42,13 +60,28 @@ proc tick*(ticker: Ticker, tickLength: float) =
 
 proc newTicker*(): Ticker =
   new result
-  result.ticks = @[]
+  result.ticks = nil
   result.lastTick = epochTime()
   result.paused = false
 
-proc add*(ticker: Ticker, tickable: Tickable) =
-  ticker.ticks.add proc(tickLength: float) =
-    tickable.tick(tickLength)
+proc add*(ticker: Ticker, tickable: Tickable): TickNode =
+  return ticker.ticks.add(proc(tickLength: float) =
+    tickable.tick(tickLength))
 
-proc add*(ticker: Ticker, tick: TickProc) =
-  ticker.ticks.add tick
+proc add*(ticker: Ticker, tick: TickProc): TickNode =
+  var newNode = TickNode(ticker: tick, next: nil)
+  ticker.last.next = newNode
+  ticker.last = newNode
+  return newNode
+
+proc delete*(ticker: Ticker, tickNode: TickNode) =
+  var lastTick: TickNode = nil
+  for tick in ticker.ticks.nodes:
+    if tick == tickNode:
+      lastTick.next = tick.next
+      if tickNode == ticker.last:
+        ticker.last = lastTick
+      if tickNode == ticker.ticks:
+        ticker.ticks = tick.next
+      return
+    lastTick = tick
